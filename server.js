ca986c09c217c706aee61bffc4e77fbfe5864fbe
Hossam Sherif <husam287@hotmail.com>
2021-05-04 02:40:32 +0200

    fix port and helmet package

--- server.js
+++ server.js
@@ -2,21 +2,25 @@ const express = require('express');
 const errorMiddelware = require('./middlewares/errors');
 const allowHeaders = require('./middlewares/headers');
 const bodyParser = require('body-parser');
+const helmet = require("helmet");
 
 const webScrapRouter = require('./routes/webscrap');
 
+
 const app = express();
+
+app.use(helmet());
+app.use(allowHeaders);
 app.use(bodyParser.json());
 
-app.use(allowHeaders);
 
 
-app.use('/api/shein',webScrapRouter);
+app.use('/api/shein', webScrapRouter);
+
 
 
 app.use(errorMiddelware);
-
-app.listen(8080, () => {
+app.listen(process.env.PORT || 8080, () => {
     console.log('Example app listening on port port!');
 });
 
